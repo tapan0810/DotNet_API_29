c5df863669829ca06cd73ca084816b4941c34aee
tapan0810 <arceus0810@gmail.com>
2026-04-21 22:05:52 +0530

    feat : Implemented Pg Model

---
+++ DotNet_API_29/Entities/Models/Pg.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNet_API_29.Entities.Models
+{
+    public class Pg
+    {
+        public int Id { get; set; }
+        public string PgName { get; set; } = string.Empty;
+        public string PgDescription { get; set; } = string.Empty;
+        public string PgLocation { get; set; } = string.Empty;
+        public string PgContact { get; set; } = string.Empty;
+        public string PgOwner { get; set; } = string.Empty;
+    }
+}
