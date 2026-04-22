bce958d8d31b40e28c96e483bcb3260f1e1d3c5e
tapan0810 <arceus0810@gmail.com>
2026-04-22 23:07:56 +0530

    feat : Implemented Dtos with required Annotaions

---
+++ DotNet_API_29/Entities/DTOs/CreatePgDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_API_29.Entities.DTOs
+{
+    public class CreatePgDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string PgName { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(100)]
+        public string PgDescription { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(100)]
+        public string PgLocation { get; set; } = string.Empty;
+        [Required]
+        [Phone]
+        public string PgContact { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(50)]
+        public string PgOwner { get; set; } = string.Empty;
+    }
+}
