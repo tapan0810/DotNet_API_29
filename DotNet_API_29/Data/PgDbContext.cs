c279c3300bc11af2b38a5792d7acb21705c5a45d
tapan0810 <arceus0810@gmail.com>
2026-04-24 23:25:22 +0530

    feat : Done with Data Migration (Code first Approach)

---
+++ DotNet_API_29/Data/PgDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DotNet_API_29.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet_API_29.Data
+{
+    public class PgDbContext:DbContext
+    {
+        public PgDbContext(DbContextOptions<PgDbContext> options):base(options)
+        {
+        }
+        public DbSet<Pg> Pgs => Set<Pg>();
+    }
+}
---
+++ DotNet_API_29/Migrations/20260424175431_Initial Create.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNet_API_29.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pgs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PgName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PgDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PgLocation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PgContact = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PgOwner = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pgs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pgs");
+        }
+    }
+}
