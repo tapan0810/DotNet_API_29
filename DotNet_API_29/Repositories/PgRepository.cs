fa74bf0773e2fe31b9753beb1fa4d08cc0f09ee0
tapan0810 <arceus0810@gmail.com>
2026-04-25 18:43:59 +0530

    feat : Implemented PgRepository

---
+++ DotNet_API_29/Repositories/PgRepository.cs
@@ -0,0 +1,36 @@
+﻿using DotNet_API_29.Data;
+using DotNet_API_29.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DotNet_API_29.Repositories
+{
+    public class PgRepository (PgDbContext _context): IPgRepositrory
+    {
+        public async Task AddPg(Pg pg)
+        {
+            await _context.Pgs.AddAsync(pg);
+        }
+
+        public void DeletePg(int id)
+        {
+            var pg = _context.Pgs.FirstOrDefault(x => x.Id == id);
+            _context.Pgs.Remove(pg);
+        }
+
+        public async Task<IEnumerable<Pg>> GetAllPg()
+        {
+            return await _context.Pgs.ToListAsync();
+        }
+
+        public async Task<Pg> GetPgById(int id)
+        {
+            return await _context.Pgs.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public void UpdatePg(int id, Pg pg)
+        {
+           _context.Pgs.Update(pg);
+        }
+    }
+}
