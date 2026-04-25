90f8695bb59dce9f64a3d4e38f2f4a1e83309c37
tapan0810 <arceus0810@gmail.com>
2026-04-25 18:33:19 +0530

    feat : Implemented Pg Repository Interface

---
+++ DotNet_API_29/Repositories/IPgRepositrory.cs
@@ -0,0 +1,14 @@
+﻿using DotNet_API_29.Entities.DTOs;
+using DotNet_API_29.Entities.Models;
+
+namespace DotNet_API_29.Repositories
+{
+    public interface IPgRepositrory
+    {
+        Task<IEnumerable<Pg>> GetAllPg();
+        Task<Pg> GetPgById(int id);
+        Task AddPg(Pg pg);
+        void UpdatePg(int id, Pg pg);
+        void DeletePg(int id);
+    }
+}
