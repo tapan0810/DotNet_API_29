1440d554da81dbfdddb664239d5cab6ca85a3172
tapan0810 <arceus0810@gmail.com>
2026-04-25 22:57:49 +0530

    feat : Impelmented Custom Exception for Pg

---
+++ DotNet_API_29/Exceptions/PgException.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNet_API_29.Exceptions
+{
+    public class PgException:Exception
+    {
+        public int StatusCode { get; }
+
+        protected PgException(string message ,int statusCode):base (message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
