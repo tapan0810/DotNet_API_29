4e68e8ab388ed74d050d07619300b983b07e0875
tapan0810 <arceus0810@gmail.com>
2026-04-26 23:03:24 +0530

    feat : Implemented AppException & PgNotFoundException

---
+++ DotNet_API_29/Exceptions/PgNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNet_API_29.Exceptions
+{
+    public class PgNotFoundException:AppException
+    {
+        public PgNotFoundException(int id) : base($"Pg with id {id} not found", StatusCodes.Status404NotFound) { }
+    }
+}
