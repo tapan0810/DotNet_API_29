3e18e6842065dac7639ded41da3da0c1321f21f5
tapan0810 <arceus0810@gmail.com>
2026-04-26 23:09:27 +0530

    feat : Implemented DuplicatePgException

---
+++ DotNet_API_29/Exceptions/DuplicatePgException.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNet_API_29.Exceptions
+{
+    public class DuplicatePgException:AppException
+    {
+        public DuplicatePgException(string name)
+            :base($"Pg with name {name} already exist.",StatusCodes.Status409Conflict)
+        {
+
+        }
+    }
+}
